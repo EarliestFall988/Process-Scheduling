e1115da4309fa6bce4b44b1333ebc5008568daac
Amy Neeland <aneeland1@selene.beocat.ksu.edu>
2023-02-20 17:57:10 -0600

    FCFS scheduling

--- hw2/hw2/src/process_scheduling.c
+++ hw2/hw2/src/process_scheduling.c
@@ -20,6 +20,36 @@ void virtual_cpu(ProcessControlBlock_t *process_control_block)
 
 bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
+    /*//Sort array by arrival time first 
+    float completion_time[dyn_array_size(ready_queue)];
+    float waiting_time[dyn_array_size(ready_queue) + 1];
+    wating_time[0] = 0;
+    for(int i = 0;i < dyn_array_size(ready_queue);i++)
+    {
+        ProcessControlBlock_t * current = dyn_array_at(ready_queue, 0);
+        completion_time[i] = current->remaining_burst_time;
+        wating_time[i+1] = wating_time[i] + completion_time[i];
+        while(current->remaining_burst_time != 0)
+        {
+            virtual_cpu(current);
+        }
+        dyn_array_pop_front(dyn_array_t *const dyn_array);
+    }
+    
+    //Calculate values for result
+    float average_waiting_time;
+    float average_turnaround_time;
+    unsigned long total_run_time = 0;
+    for(int i = 0;i < sizeof(waiting_time);i++)
+    {
+        average_wating_time += waiting_time[i];
+        total_run_time += completion_time[i];
+    }
+    
+    result->average_waiting_time = average_waiting_time/(sizeof(waiting_time) - 1);
+    result->average_turnaround_time = (flaot)total_run_time/(sizeof(waiting_time) - 1);
+    result->total_run_time = total_run_time;*/
+
     UNUSED(ready_queue);
     UNUSED(result);
     return false;
