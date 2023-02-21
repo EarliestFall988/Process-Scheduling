4d935d3224be7054a368ad1488f1f6769450fa33
Amy Neeland <aneeland1@selene.beocat.ksu.edu>
2023-02-20 18:57:53 -0600

    I'm claiming functions

--- hw2/hw2/src/process_scheduling.c
+++ hw2/hw2/src/process_scheduling.c
@@ -6,22 +6,21 @@
 #include "dyn_array.h"
 #include "processing_scheduling.h"
 
+
 // You might find this handy.  I put it around unused parameters, but you should
 // remove it before you submit. Just allows things to compile initially.
 #define UNUSED(x) (void)(x)
 
 // private function
-void virtual_cpu(ProcessControlBlock_t *process_control_block)
+void virtual_cpu(ProcessControlBlock_t *process_control_block) 
 {
     // decrement the burst time of the pcb
     --process_control_block->remaining_burst_time;
 }
 
-bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result)
+bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
-    // Sort array by arrival time first
-
-    /*
+    /*//Sort array by arrival time first 
     float completion_time[dyn_array_size(ready_queue)];
     float waiting_time[dyn_array_size(ready_queue) + 1];
     wating_time[0] = 0;
@@ -36,7 +35,7 @@ bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result)
         }
         dyn_array_pop_front(dyn_array_t *const dyn_array);
     }
-
+    
     //Calculate values for result
     float average_waiting_time;
     float average_turnaround_time;
@@ -46,34 +45,33 @@ bool first_come_first_serve(dyn_array_t *ready_queue, ScheduleResult_t *result)
         average_wating_time += waiting_time[i];
         total_run_time += completion_time[i];
     }
-
+    
     result->average_waiting_time = average_waiting_time/(sizeof(waiting_time) - 1);
-    result->average_turnaround_time = (float)total_run_time/(sizeof(waiting_time) - 1);
-    result->total_run_time = total_run_time;
-
-
-    */
+    result->average_turnaround_time = (flaot)total_run_time/(sizeof(waiting_time) - 1);
+    result->total_run_time = total_run_time;*/
 
     UNUSED(ready_queue);
     UNUSED(result);
     return false;
 }
 
-bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result)
+bool shortest_job_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
+    //Amy to work on
     UNUSED(ready_queue);
     UNUSED(result);
-    return false;
+    return false;   
 }
 
-bool priority(dyn_array_t *ready_queue, ScheduleResult_t *result)
+bool priority(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
+    //Amy to work on
     UNUSED(ready_queue);
     UNUSED(result);
-    return false;
+    return false;   
 }
 
-bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quantum)
+bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quantum) 
 {
     UNUSED(ready_queue);
     UNUSED(result);
@@ -81,13 +79,13 @@ bool round_robin(dyn_array_t *ready_queue, ScheduleResult_t *result, size_t quan
     return false;
 }
 
-dyn_array_t *load_process_control_blocks(const char *input_file)
+dyn_array_t *load_process_control_blocks(const char *input_file) 
 {
     UNUSED(input_file);
     return NULL;
 }
 
-bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result)
+bool shortest_remaining_time_first(dyn_array_t *ready_queue, ScheduleResult_t *result) 
 {
     UNUSED(ready_queue);
     UNUSED(result);
