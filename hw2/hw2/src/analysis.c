174e4afcabf58c54916a52ba21225d40e7b857d2
twh@ksu.edu <twh@ksu.edu>
2023-02-26 19:07:19 -0600

    fixing analysis

--- hw2/hw2/src/analysis.c
+++ hw2/hw2/src/analysis.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #include "../include/dyn_array.h"
 #include "../include/processing_scheduling.h"
-
 
 #define FCFS "FCFS"
 #define P "P"
@@ -13,47 +11,70 @@
 
 // Add and comment your analysis code in this function.
 // THIS IS NOT FINISHED.
-int main(int argc, char **argv) 
+int main(int argc, char **argv)
 {
-    if (argc < 3) 
+    if (argc < 3)
     {
         printf("%s <pcb file> <schedule algorithm> [quantum]\n", argv[0]);
         return EXIT_FAILURE;
     }
-    ScheduleResult_t * results = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
-    dyn_array_t * PCB_Array = load_process_control_blocks("argv[0]");
 
-    if(strcmp(argv[1],FCFS))
+    // ScheduleResult_t * results = (ScheduleResult_t *)malloc(sizeof(ScheduleResult_t));
+    ScheduleResult_t *results = {0};
+
+    printf("Loading PCBs from %s \n", argv[1]);
+
+    dyn_array_t *PCB_Array = load_process_control_blocks(argv[1]);
+
+    printf("Number of PCBs: %lu \n", dyn_array_size(PCB_Array));
+
+    printf("args 2: %s \n", argv[2]);
+
+    if (strcmp(argv[2], FCFS) == 0)
     {
-        first_come_first_serve(PCB_Array,results);
+        first_come_first_serve(PCB_Array, results);
         printf("Average Waiting Time: %f \n", results->average_waiting_time);
         printf("Average Turnaround Time: %f \n", results->average_turnaround_time);
         printf("Total Run Time: %lu \n", results->total_run_time);
     }
-    if(strcmp(argv[1],P))
+    if (strcmp(argv[2], P) == 0)
     {
-        priority(PCB_Array,results);
+        priority(PCB_Array, results);
         printf("Average Waiting Time: %f \n", results->average_waiting_time);
         printf("Average Turnaround Time: %f \n", results->average_turnaround_time);
         printf("Total Run Time: %lu \n", results->total_run_time);
     }
-    if(strcmp(argv[1],RR))
+    if (strcmp(argv[2], RR) == 0)
     {
-        round_robin(PCB_Array,results,(size_t)atoi(argv[2]));
+        printf("args 2: %s \n", argv[2]);
+        printf("args 3: %s \n", argv[3]);
+
+        size_t quantum = (size_t)atoi(argv[3]);
+        printf("quantum created: %lu \n", quantum);
+
+        bool x = round_robin(PCB_Array, results, quantum);
+
+        if (!x)
+        {
+            printf("Error: Round Robin failed \n");
+            return EXIT_FAILURE;
+        }
+
+        printf("args 2: %s \n", argv[2]);
         printf("Average Waiting Time: %f \n", results->average_waiting_time);
         printf("Average Turnaround Time: %f \n", results->average_turnaround_time);
         printf("Total Run Time: %lu \n", results->total_run_time);
     }
-    if(strcmp(argv[1],SJF))
+    if (strcmp(argv[2], SJF) == 0)
     {
-        shortest_job_first(PCB_Array,results);
+        shortest_job_first(PCB_Array, results);
         printf("Average Waiting Time: %f \n", results->average_waiting_time);
         printf("Average Turnaround Time: %f \n", results->average_turnaround_time);
         printf("Total Run Time: %lu \n", results->total_run_time);
     }
+
     free(results);
-    free(PCB_Array);
-
+    dyn_array_destroy(PCB_Array);
 
     return EXIT_SUCCESS;
 }
